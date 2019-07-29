5e0cb935c0bbef8a18f574a09c26e8d8ca0081fe
tonyanjo <tonyestulano@hotmail.com>
2019-07-29 18:11:32 -0300

    Adicionado alguns comentários guia

--- public_html/algorítimoDeFogo/fire.js
+++ public_html/algorítimoDeFogo/fire.js
@@ -2,24 +2,25 @@ const firePixelsArray = []
 const fireWidth = 40
 const fireHeight = 40
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+// Array das 37 cores que compôem o effeito de fogo
 
-function start(){
-    createFireDataStructure()
-    createFireSource()
-    renderFire()
+function start(){               //Function de execução ... dããã
+    createFireDataStructure()   //Função da estrutura da grade de fogo
+    createFireSource()          //Function que cria uma base mestre para se tornar guia para o primeiro pixel olhar para baixo e seguir
+    renderFire()                //Function que injeta os comandos html para ter um retorno visual
 
-    setInterval(calculateFirePropagation, 50) //Velocidade de Execução do Algorítimo
+    setInterval(calculateFirePropagation, 50)       //Velocidade de Execução do Algorítimo
 }
 
-function createFireDataStructure() {
-    const numberOfPixels = fireWidth * fireHeight //Estrutura da grade... sim... é só isso... largura * altura kkkkkkk
+function createFireDataStructure() { 
+    const numberOfPixels = fireWidth * fireHeight   //Estrutura da grade... sim... é só isso... largura * altura kkkkkkk
 
-    for (let i = 0; i < numberOfPixels; i++) { //For para criar um guia com os índices do Array
+    for (let i = 0; i < numberOfPixels; i++) {      //For para criar um guia com os índices do Array
         firePixelsArray[i] = 0
     }
 }
 
-function calculateFirePropagation() {
+function calculateFirePropagation() {     //Função que determina qual o valor interno do array
     for (let column = 0; column < fireWidth; column++) {
         for (let row = 0; row < fireHeight; row++) {
             const pixelIndex = column + ( fireWidth * row )
@@ -31,14 +32,14 @@ function calculateFirePropagation() {
     renderFire()
 }
 
-function updateFireIntensityPerPixel(currentPixelIndex) {
+function updateFireIntensityPerPixel(currentPixelIndex) {   //Função que faz o pixel de cima ter sua intensidade determinada de acordo com a intencidade do pixel de baixo
     const belowPixelIndex = currentPixelIndex + fireWidth
     if (belowPixelIndex >= fireWidth * fireHeight) {
         return
     }
 
     const decay = Math.floor(Math.random()*3) 
-    //Randomizar o decay da o efeito de fogo, se deixar o decay fixo ele simplesmente permanece uma atualização estática
+    //Randomizar o decay da o efeito de fogo, se deixar o decay fixo ele simplesmente permanece uma atualização estática, quanto maior a aleatoriedade maior a dissipação do fogo
     
     const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
     const newFireIntensity = 
